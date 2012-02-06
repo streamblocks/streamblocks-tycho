8225bac8b29649ef5f5727a6f9d8144cb2950796
Jörn W. Janneck <jwj@cs.lth.se>
2012-02-06 14:39:02 +0000

    .

--- source/java/net/opendf/ir/common/Namespace.java
+++ source/java/net/opendf/ir/common/Namespace.java
@@ -1,7 +1,86 @@
 package net.opendf.ir.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.opendf.ir.AbstractIRNode;
 
+/**
+ * Namespaces impose a hierarchical structure onto the global {@link Decl declarations}. Except for the singular top-level namespace,
+ * namespaces are contained within a parent, and each namespace can contain any number of subnamespaces, which all have to have distinct 
+ * <it>relative names</it>.
+ * 
+ * The full name of a namespace is a sequence of names which is its relative name appended to the full name of its parent. The relative 
+ * name of the top level namespace is <tt>null</tt>, and its full name is the empty sequence.
+ * 
+ * Namespaces contain {@link NamespaceDecl namespace declarations}, which in turn contain the actual declarations that are contributed to
+ * the namespace. This indirection exists so that local accessibility of a declaration can be properly implemented.
+ * 
+ * 
+ * @author Jorn W. Janneck <jwj@acm.org>
+ *
+ */
+
 public class Namespace extends AbstractIRNode {
+	
+	public Namespace  getParent() {
+		return parent;
+	}
+	
+	public String  getRelativeName() {
+		return relativeName;
+	}
+	
+	public String []  getFullName() {
+		if (parent == null) {
+			return new String[0];
+		} else {
+			String [] nm = parent.getFullName();
+			String [] fullName = new String [nm.length + 1];
+			for (int i = 0; i < nm.length; i++) {
+				fullName[i] = nm[i];
+			}
+			fullName[nm.length] = relativeName;
+			return fullName;
+		}
+	}
+	
+	public List<NamespaceDecl>  getDecls() {
+		return decls;
+	}
+	
+	public List<Namespace>  getSubnamespaces() {
+		return subnamespaces;
+	}
+
+	void  addDecl(NamespaceDecl decl) { 
+		decls.add(decl);
+	}
+	
+	void  addSubnamespace(Namespace sub) {
+		subnamespaces.add(sub);
+	}
+	
+	//
+	//  Ctor
+	//
+	
+	public Namespace(Namespace parent, String relativeName) {
+		this.parent = parent;
+		if (parent != null)
+			parent.addSubnamespace(this);
+		this.relativeName = relativeName;
+		decls = new ArrayList<NamespaceDecl>();
+	}
+		
+	private Namespace 				parent;
+	private String 					relativeName;
+	private List<NamespaceDecl>		decls;
+	private List<Namespace>			subnamespaces;
+	
+		
+	public static Namespace createTopLevelNamespace() {
+		return new Namespace(null, null);
+	}
 
 }
--- source/java/net/opendf/ir/common/NamespaceDecl.java
+++ source/java/net/opendf/ir/common/NamespaceDecl.java
@@ -1,43 +1,58 @@
 package net.opendf.ir.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.opendf.ir.AbstractIRNode;
 
-public class NamespaceDecl extends AbstractIRNode {
-	
-	public NamespaceDecl  getParent() {
-		return parent;
-	}
-	
-	public String  getRelativeName() {
-		return relativeName;
-	}
-	
-	public String []  getFullName() {
-		if (parent == null) {
-			return new String[0];
-		} else {
-			String [] nm = parent.getFullName();
-			String [] fullName = new String [nm.length + 1];
-			for (int i = 0; i < nm.length; i++) {
-				fullName[i] = nm[i];
-			}
-			fullName[nm.length] = relativeName;
-			return fullName;
-		}
-	}
-	
-	public NamespaceDecl(NamespaceDecl parent, String relativeName) {
-		this.parent = parent;
-		this.relativeName = relativeName;
-	}
-		
-	private NamespaceDecl 	parent;
-	private String 		relativeName;
-	
-	
-	
-	public static NamespaceDecl createTopLevelNamespace() {
-		return new NamespaceDecl(null, null);
-	}
+/**
+ * A namespace declaration represents a lexical entity that contributes {@link Decl declarations} to a namespace, and which also serves as the 
+ * scope of any of its declaration with 'local' accessibility.
+ * 
+ * In addition to any number of (variable, type, and entity) declarations, it may also contain other namespace declarations.
+ * 
+ * @author Jorn W. Janneck <jwj@acm.org>
+ *
+ */
 
+public class NamespaceDecl extends AbstractIRNode {
+
+	
+	public List<Decl>  getDecls() {
+		return decls;
+	}
+	
+	public List<NamespaceDecl>  getSubnamespaceDecls() {
+		return subdecls;
+	}
+	
+	public Namespace  getNamespace() {
+		return namespace;
+	}
+	
+	
+	//
+	
+	void  addDecl(Decl d) {
+		decls.add(d);
+	}
+	
+	void  addSubnamespaceDecl(NamespaceDecl nd) {
+		subdecls.add(nd);
+	}
+	
+	//
+	//  Ctor
+	//
+	
+	public NamespaceDecl(Namespace namespace) {
+		namespace.addDecl(this);
+		decls = new ArrayList<Decl>();
+	}
+	
+
+	private Namespace   		namespace;
+	private List<Decl>			decls;
+	private List<NamespaceDecl>	subdecls;
+	
 }
