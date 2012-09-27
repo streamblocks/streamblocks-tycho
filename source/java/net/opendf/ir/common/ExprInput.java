48181a5e397428810a5b9440f7bbebc3b6e4d046
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2012-09-27 09:45:18 +0000

    Added patternLength to ExprInput

--- source/java/net/opendf/ir/common/ExprInput.java
+++ source/java/net/opendf/ir/common/ExprInput.java
@@ -14,6 +14,8 @@ public class ExprInput extends Expression {
 	public boolean  hasRepeat() { return hasRepeat; }
 	
 	public int  getRepeat() { return repeat; }
+
+	public int  getPatternLength() { return patternLength; }
 	
 	
 	//
@@ -28,11 +30,12 @@ public class ExprInput extends Expression {
 		this (port, offset, true, repeat);
 	}
 	
-	private ExprInput(PortName port, int offset, boolean hasRepeat, int repeat) {
+	private ExprInput(PortName port, int offset, boolean hasRepeat, int repeat, int patternLength) {
 		this.port = port;
 		this.offset = offset;
 		this.hasRepeat = hasRepeat;
 		this.repeat = repeat;
+		this.patternLength = patternLength;
 	}
 	
 	
@@ -40,4 +43,5 @@ public class ExprInput extends Expression {
 	private PortName	port;
 	private int			offset;
 	private int			repeat;
+	private int			patternLength;
 }
