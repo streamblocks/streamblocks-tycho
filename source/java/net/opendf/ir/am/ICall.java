c3bc2c3e9ea7082ccf15824209a21aca466ab20c
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2013-04-10 14:40:43 +0200

    Transitions and Conditions by index
    Added a list of Transitions and a list of Conditions to ActorMachine.
    Changed Instructions to refer to Transitions and Conditions by index.

--- source/java/net/opendf/ir/am/ICall.java
+++ source/java/net/opendf/ir/am/ICall.java
@@ -1,39 +1,40 @@
 package net.opendf.ir.am;
 
 /**
- * This class represents the call instruction, which has two components: the {@link Transition transition} to be executed during the call, and the 
+ * This class represents the call instruction, which has two components: the
+ * {@link Transition transition} to be executed during the call, and the
  * controller state to go to after the call instruction completes.
  * 
- * In general, a call instruction invalidates all scopes that were defined since the last call instruction. A scope may be retained if it
- * can be shown that the transition executed during the call instruction does not affect the values of the variables defined in it.
- * 
- * <p><tt>call(tc, s)<tt>
+ * <tt>call(tc, s)<tt>
  * 
  * @author Jorn W. Janneck <jwj@acm.org>
- *
+ * 
  */
 
 public class ICall extends Instruction {
 
 	@Override
-	public <R,P> R accept(InstructionVisitor<R,P> v, P p) {
+	public <R, P> R accept(InstructionVisitor<R, P> v, P p) {
 		return v.visitCall(this, p);
 	}
-	
-	public Transition		T() { return t; }
-	
-	public int				S() { return s; }
-	
+
+	public int T() {
+		return t;
+	}
+
+	public int S() {
+		return s;
+	}
+
 	//
-	//  Ctor
+	// Ctor
 	//
-	
-	public ICall(Transition t, int s) {
+
+	public ICall(int t, int s) {
 		this.t = t;
 		this.s = s;
 	}
-	
-	
-	private Transition  	t;
-	private int             s;
+
+	private int t;
+	private int s;
 }
