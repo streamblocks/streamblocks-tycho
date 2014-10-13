e0617ebb3715305922945a683532e9a5136d1603
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2014-10-13 21:17:49 +0200

    Updated parser for Orcc and Caltoopia compatibility

--- core/src/test/java/se/lth/cs/tycho/parsing/cal/TestLegalCalFiles.java
+++ core/src/test/java/se/lth/cs/tycho/parsing/cal/TestLegalCalFiles.java
@@ -21,34 +21,34 @@ import se.lth.cs.tycho.ir.NamespaceDecl;
 @RunWith(Parameterized.class)
 public class TestLegalCalFiles {
 	private static List<Path> roots() {
-		return Arrays.asList(
-				Paths.get("../../orc-apps/RVC"),
-				Paths.get("../../opendf/models"));
+		return Arrays.asList(Paths.get("../../orc-apps/RVC"), Paths.get("../../caltoopia/org.caltoopia.tests/cal-src"));
 	}
 
 	@Parameters(name = "{index}: Parsing {0}")
 	public static List<Path[]> files() {
 		return roots().stream()
 				.flatMap(f -> getCalPaths(f))
-				.map(f -> new Path[]{ f })
+				.map(f -> new Path[] { f })
 				.collect(Collectors.toList());
 	}
 
 	private static Stream<Path> getCalPaths(Path base) {
 		try {
 			return Files.walk(base)
-					.filter(f -> f.getFileName().toString().endsWith(".cal"));
+					.filter(f -> f.getFileName()
+							.toString()
+							.endsWith(".cal"));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private final Path input;
-	
+
 	public TestLegalCalFiles(Path input) {
 		this.input = input;
 	}
-	
+
 	@Test
 	public void testParseCalFile() throws IOException, ParseException {
 		CalParser parser = new CalParser(Files.newInputStream(input));
