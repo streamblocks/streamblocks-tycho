cc2b5cfd344eee0fd6ce540a2de66bffa0a53346
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2014-10-14 13:35:02 +0200

    Removed protected API from Instantiator

--- core/src/main/java/se/lth/cs/tycho/instantiation/Instantiator.java
+++ core/src/main/java/se/lth/cs/tycho/instantiation/Instantiator.java
@@ -37,19 +37,19 @@ public class Instantiator {
 	public Instantiator(DeclarationLoader loader) {
 		this(loader, Collections.emptyList());
 	}
+
 	public Instantiator(DeclarationLoader loader, List<Transformer<State, State>> stateTransformers) {
 		this.loader = loader;
 		this.visitor = new Visitor();
 		this.translator = new ActorToActorMachine(stateTransformers);
 	}
 
-	public Instance instantiate(QID qid, List<Parameter<Type>> typeParameters,
-			List<Parameter<Value>> valueParameters) {
+	public Instance instantiate(QID qid, List<Parameter<Type>> typeParameters, List<Parameter<Value>> valueParameters) {
 		return instantiate(qid, typeParameters, valueParameters, null);
 	}
 
-	public Instance instantiate(QID qid, List<Parameter<Type>> typeParameters,
-			List<Parameter<Value>> valueParameters, NamespaceDecl location) {
+	public Instance instantiate(QID qid, List<Parameter<Type>> typeParameters, List<Parameter<Value>> valueParameters,
+			NamespaceDecl location) {
 		GlobalEntityDecl decl = loader.loadEntity(qid, location);
 		return instantiate(decl.getEntity(), typeParameters, valueParameters, loader.getLocation(decl));
 	}
@@ -64,84 +64,66 @@ public class Instantiator {
 		return entity.accept(visitor, new Context(typeParameters, valueParameters, location));
 	}
 
-	protected Instance instantiateCalActor(CalActor entity, List<Parameter<Type>> typeParameters,
-			List<Parameter<Value>> valueParameters, NamespaceDecl location) {
-		return translator.translate(entity);
-	}
-
-	protected Instance instantiateNlNetwork(NlNetwork entity, List<Parameter<Type>> typeParameters,
-			List<Parameter<Value>> valueParameters, NamespaceDecl location) {
-		throw new UnsupportedOperationException("Nl networks are not yet supported.");
-	}
-
-	protected Instance instantiateXDFNetwork(XDFNetwork entity, List<Parameter<Type>> typeParameters,
-			List<Parameter<Value>> valueParameters, NamespaceDecl location) {
-		ImmutableList.Builder<Node> nodeBuilder = ImmutableList.builder();
-		for (XDFInstance inst : entity.getInstances()) {
-			Instance result = instantiate(inst.getEntity(), ImmutableList.empty(), ImmutableList.empty(), location);
-			nodeBuilder.add(new Node(inst.getName(), result, ImmutableList.empty()));
-		}
-		ImmutableList<Node> nodes = nodeBuilder.build();
-		ImmutableList.Builder<Connection> connBuilder = ImmutableList.builder();
-		for (XDFConnection conn : entity.getConnections()) {
-			Identifier src = findNode(conn.getSourceInstance(), nodes);
-			Identifier dst = findNode(conn.getDestinaitonInstance(), nodes);
-			connBuilder.add(new Connection(src, conn.getSourcePort(), dst, conn.getDestinationPort(),
-					ImmutableList.empty()));
-		}
-		ImmutableList.Builder<PortDecl> inputPorts = ImmutableList.builder();
-		for (PortDecl in : entity.getInputPorts()) {
-			inputPorts.add(in.copy(in.getName(), in.getType()));
-		}
-		ImmutableList.Builder<PortDecl> outputPorts = ImmutableList.builder();
-		for (PortDecl out : entity.getOutputPorts()) {
-			outputPorts.add(out.copy(out.getName(), out.getType()));
-		}
-		return new Network(nodes, connBuilder.build(), inputPorts.build(), outputPorts.build());
-	}
-
-	protected Instance instantiateGlobalEntityReference(GlobalEntityReference entity,
-			List<Parameter<Type>> typeParameters, List<Parameter<Value>> valueParameters,
-			NamespaceDecl location) {
-		if (entity.isNamespaceReference()) {
-			throw new Error("Can not create an entity instance of a global namespace reference.");
-		}
-		return instantiate(entity.getQualifiedIdentifier(), typeParameters, valueParameters, location);
-	}
-
-	private Identifier findNode(String name, List<Node> nodes) {
-		if (name == null || name.isEmpty()) {
-			return null;
-		}
-		for (Node node : nodes) {
-			if (node.getName()
-					.equals(name)) {
-				return node.getIdentifier();
-			}
-		}
-		throw new RuntimeException("Unknown node: " + name);
-	}
-
 	private class Visitor implements EntityVisitor<Instance, Context> {
 		@Override
 		public Instance visitCalActor(CalActor entity, Context param) {
-			return instantiateCalActor(entity, param.typeParameters, param.valueParameters, param.location);
+			return translator.translate(entity);
 		}
 
 		@Override
 		public Instance visitNlNetwork(NlNetwork entity, Context param) {
-			return instantiateNlNetwork(entity, param.typeParameters, param.valueParameters, param.location);
+			throw new UnsupportedOperationException("Nl networks are not yet supported.");
 		}
 
 		@Override
 		public Instance visitGlobalEntityReference(GlobalEntityReference entity, Context param) {
-			return instantiateGlobalEntityReference(entity, param.typeParameters, param.valueParameters, param.location);
+			if (entity.isNamespaceReference()) {
+				throw new Error("Can not create an entity instance of a global namespace reference.");
+			}
+			return instantiate(entity.getQualifiedIdentifier(), param.typeParameters, param.valueParameters,
+					param.location);
 		}
 
 		@Override
 		public Instance visitXDFNetwork(XDFNetwork entity, Context param) {
-			return instantiateXDFNetwork(entity, param.typeParameters, param.valueParameters, param.location);
+			ImmutableList.Builder<Node> nodeBuilder = ImmutableList.builder();
+			for (XDFInstance inst : entity.getInstances()) {
+				Instance result = instantiate(inst.getEntity(), ImmutableList.empty(), ImmutableList.empty(),
+						param.location);
+				nodeBuilder.add(new Node(inst.getName(), result, ImmutableList.empty()));
+			}
+			ImmutableList<Node> nodes = nodeBuilder.build();
+			ImmutableList.Builder<Connection> connBuilder = ImmutableList.builder();
+			for (XDFConnection conn : entity.getConnections()) {
+				Identifier src = findNode(conn.getSourceInstance(), nodes);
+				Identifier dst = findNode(conn.getDestinaitonInstance(), nodes);
+				connBuilder.add(new Connection(src, conn.getSourcePort(), dst, conn.getDestinationPort(),
+						ImmutableList.empty()));
+			}
+			ImmutableList.Builder<PortDecl> inputPorts = ImmutableList.builder();
+			for (PortDecl in : entity.getInputPorts()) {
+				inputPorts.add(in.copy(in.getName(), in.getType()));
+			}
+			ImmutableList.Builder<PortDecl> outputPorts = ImmutableList.builder();
+			for (PortDecl out : entity.getOutputPorts()) {
+				outputPorts.add(out.copy(out.getName(), out.getType()));
+			}
+			return new Network(nodes, connBuilder.build(), inputPorts.build(), outputPorts.build());
 		}
+
+		private Identifier findNode(String name, List<Node> nodes) {
+			if (name == null || name.isEmpty()) {
+				return null;
+			}
+			for (Node node : nodes) {
+				if (node.getName()
+						.equals(name)) {
+					return node.getIdentifier();
+				}
+			}
+			throw new RuntimeException("Unknown node: " + name);
+		}
+
 	}
 
 	private static class Context {
