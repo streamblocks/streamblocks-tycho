ecafb6de6817f4049fc6dd58085186ba95e0e002
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2016-01-08 17:03:01 +0100

    Moved submodule old-controller-stuff to own project

--- old-controller-stuff/src/main/java/se/lth/cs/tycho/transform/Transformation.java
+++
@@ -1,7 +0,0 @@
-package se.lth.cs.tycho.transform;
-
-import java.util.function.Function;
-
-public interface Transformation<T> extends Function<T, T> {
-
-}
--- old-controller-stuff/src/main/java/se/lth/cs/tycho/transform/reduction/util/ControllerWrapper.java
+++
@@ -1,7 +0,0 @@
-package se.lth.cs.tycho.transform.reduction.util;
-
-import se.lth.cs.tycho.transform.util.Controller;
-
-public interface ControllerWrapper<A, B> {
-	public Controller<B> wrap(Controller<A> original);
-}
