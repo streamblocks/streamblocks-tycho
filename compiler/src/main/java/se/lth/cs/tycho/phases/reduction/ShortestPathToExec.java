79351a4397918488cf72430da0456ff4561a577c
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2016-12-03 11:59:46 +0100

    Reverts shortest-path-to-exec reduction to full search

--- compiler/src/main/java/se/lth/cs/tycho/phases/reduction/ShortestPathToExec.java
+++ compiler/src/main/java/se/lth/cs/tycho/phases/reduction/ShortestPathToExec.java
@@ -23,25 +23,20 @@ public class ShortestPathToExec implements Function<State, State> {
 				return i;
 			} else if (i instanceof Test) {
 				Test t = (Test) i;
-				if (nextLevel.size() <= 5) {
-					nextLevel.put(t.targetTrue(), t);
-					nextLevel.put(t.targetFalse(), t);
-				}
+				nextLevel.put(t.targetTrue(), t);
+				nextLevel.put(t.targetFalse(), t);
 			}
 		}
 		Instruction result;
 		if (nextLevel.isEmpty()) {
 			result = s.getInstructions().get(0);
 		} else {
-			result = minLength(nextLevel, 1);
+			result = minLength(nextLevel);
 		}
 		return result;
 	}
 
-	private Instruction minLength(Map<State, Instruction> states, int depth) {
-		if (depth >= 4) {
-			return states.values().iterator().next();
-		}
+	private Instruction minLength(Map<State, Instruction> states) {
 		Map<State, Instruction> nextLevel = new HashMap<>();
 		Map<Instruction, State> origin = new HashMap<>();
 		for (State state : states.keySet()) {
@@ -60,7 +55,7 @@ public class ShortestPathToExec implements Function<State, State> {
 		if (nextLevel.isEmpty()) {
 			result = states.values().iterator().next();
 		} else {
-			State state = origin.get(minLength(nextLevel, depth + 1));
+			State state = origin.get(minLength(nextLevel));
 			result = states.get(state);
 		}
 		return result;
