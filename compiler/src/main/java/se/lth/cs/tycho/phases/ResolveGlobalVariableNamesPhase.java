495c75078cb9e2f5bd74b189f9b9798e0ef0db2d
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2017-11-20 21:34:21 +0100

    Removes one dependency on class Tree
    Removes dependency on class Tree from ResolveGlobalVariablesNamesPhase

--- compiler/src/main/java/se/lth/cs/tycho/phases/ResolveGlobalVariableNamesPhase.java
+++ compiler/src/main/java/se/lth/cs/tycho/phases/ResolveGlobalVariableNamesPhase.java
@@ -1,15 +1,21 @@
 package se.lth.cs.tycho.phases;
 
+import org.multij.Binding;
+import org.multij.BindingKind;
+import org.multij.Module;
+import org.multij.MultiJ;
 import se.lth.cs.tycho.comp.CompilationTask;
 import se.lth.cs.tycho.comp.Context;
-import se.lth.cs.tycho.decoration.Namespaces;
-import se.lth.cs.tycho.decoration.Tree;
-import se.lth.cs.tycho.decoration.VariableDeclarations;
 import se.lth.cs.tycho.ir.IRNode;
+import se.lth.cs.tycho.ir.NamespaceDecl;
 import se.lth.cs.tycho.ir.QID;
+import se.lth.cs.tycho.ir.decl.GlobalVarDecl;
 import se.lth.cs.tycho.ir.decl.VarDecl;
 import se.lth.cs.tycho.ir.expr.ExprGlobalVariable;
 import se.lth.cs.tycho.ir.expr.ExprVariable;
+import se.lth.cs.tycho.ir.expr.Expression;
+import se.lth.cs.tycho.phases.attributes.AttributeManager;
+import se.lth.cs.tycho.phases.attributes.VariableDeclarations;
 
 import java.util.Optional;
 
@@ -21,22 +27,52 @@ public class ResolveGlobalVariableNamesPhase implements Phase {
 
 	@Override
 	public CompilationTask execute(CompilationTask task, Context context) {
-		return (CompilationTask) Tree.of(task).transformNodes(this::resolve);
+		Transformation transformation = MultiJ.from(Transformation.class)
+				.bind("task").to(task)
+				.bind("attributes").to(context.getAttributeManager())
+				.instance();
+		return transformation.transform(task);
 	}
 
-	private IRNode resolve(Tree<? extends IRNode> tree) {
-		return tree.tryCast(ExprVariable.class)
-				.flatMap(this::getDeclaration)
-				.flatMap(this::getGlobalName)
-				.<IRNode> map(ExprGlobalVariable::new)
-				.orElse(tree.node());
-	}
+	@Module
+	interface Transformation {
+		@Binding(BindingKind.INJECTED)
+		CompilationTask task();
 
-	private Optional<Tree<? extends VarDecl>> getDeclaration(Tree<ExprVariable> var) {
-		return VariableDeclarations.getDeclaration(var.child(ExprVariable::getVariable));
-	}
+		@Binding(BindingKind.INJECTED)
+		AttributeManager attributes();
 
-	private Optional<QID> getGlobalName(Tree<? extends VarDecl> var) {
-		return Namespaces.globalName(var);
+		@Binding(BindingKind.LAZY)
+		default VariableDeclarations variables() {
+			return attributes().getAttributeModule(VariableDeclarations.key, task());
+		}
+
+		@Binding(BindingKind.LAZY)
+		default TreeShadow tree() {
+			return attributes().getAttributeModule(TreeShadow.key, task());
+		}
+
+		default IRNode transform(IRNode node) {
+			return node.transformChildren(this::transform);
+		}
+
+		default CompilationTask transform(CompilationTask task) {
+			return task.transformChildren(this::transform);
+		}
+
+		default Expression transform(ExprVariable var) {
+			VarDecl decl = variables().declaration(var);
+			Optional<QID> globalName = globalName(decl);
+			return globalName.<Expression>map(ExprGlobalVariable::new).orElse(var);
+		}
+
+		default Optional<QID> globalName(VarDecl decl) {
+			return Optional.empty();
+		}
+
+		default Optional<QID> globalName(GlobalVarDecl decl) {
+			NamespaceDecl ns = (NamespaceDecl) tree().parent(decl);
+			return Optional.of(ns.getQID().concat(QID.of(decl.getName())));
+		}
 	}
 }
