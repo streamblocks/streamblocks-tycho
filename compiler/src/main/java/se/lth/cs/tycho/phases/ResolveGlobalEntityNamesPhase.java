113bf7d2d77abec8d06074c958776d58c9a7d670
Gustav Cedersjö <gustav.cedersjo@cs.lth.se>
2017-11-21 21:59:57 +0100

    Removes dependency on Tree from ResolveGlobalEntityNamesPhase

--- compiler/src/main/java/se/lth/cs/tycho/phases/ResolveGlobalEntityNamesPhase.java
+++ compiler/src/main/java/se/lth/cs/tycho/phases/ResolveGlobalEntityNamesPhase.java
@@ -1,13 +1,21 @@
 package se.lth.cs.tycho.phases;
 
+import org.multij.Binding;
+import org.multij.BindingKind;
+import org.multij.Module;
+import org.multij.MultiJ;
 import se.lth.cs.tycho.comp.CompilationTask;
 import se.lth.cs.tycho.comp.Context;
-import se.lth.cs.tycho.decoration.EntityDeclarations;
-import se.lth.cs.tycho.decoration.Namespaces;
-import se.lth.cs.tycho.decoration.Tree;
 import se.lth.cs.tycho.ir.IRNode;
+import se.lth.cs.tycho.ir.QID;
+import se.lth.cs.tycho.ir.decl.GlobalEntityDecl;
+import se.lth.cs.tycho.ir.entity.nl.EntityReference;
 import se.lth.cs.tycho.ir.entity.nl.EntityReferenceGlobal;
 import se.lth.cs.tycho.ir.entity.nl.EntityReferenceLocal;
+import se.lth.cs.tycho.phases.attributes.EntityDeclarations;
+import se.lth.cs.tycho.phases.attributes.GlobalNames;
+
+import java.util.Optional;
 
 public class ResolveGlobalEntityNamesPhase implements Phase {
 	@Override
@@ -17,14 +25,30 @@ public class ResolveGlobalEntityNamesPhase implements Phase {
 
 	@Override
 	public CompilationTask execute(CompilationTask task, Context context) {
-		return (CompilationTask) Tree.of(task).transformNodes(this::resolve);
+		Transformation transformation = MultiJ.from(Transformation.class)
+				.bind("entities").to(context.getAttributeManager().getAttributeModule(EntityDeclarations.key, task))
+				.bind("globalNames").to(context.getAttributeManager().getAttributeModule(GlobalNames.key, task))
+				.instance();
+		return task.transformChildren(transformation);
 	}
 
-	private IRNode resolve(Tree<? extends IRNode> tree) {
-		return tree.tryCast(EntityReferenceLocal.class)
-				.flatMap(EntityDeclarations::getLocalDeclaration)
-				.flatMap(Namespaces::globalName)
-				.<IRNode> map(EntityReferenceGlobal::new)
-				.orElse(tree.node());
+	@Module
+	interface Transformation extends IRNode.Transformation {
+		@Binding(BindingKind.INJECTED)
+		EntityDeclarations entities();
+
+		@Binding(BindingKind.INJECTED)
+		GlobalNames globalNames();
+
+		@Override
+		default IRNode apply(IRNode node) {
+			return node.transformChildren(this);
+		}
+
+		default EntityReference apply(EntityReferenceLocal ref) {
+			GlobalEntityDecl entity = entities().declaration(ref);
+			Optional<QID> name = globalNames().globalName(entity);
+			return name.<EntityReference>map(EntityReferenceGlobal::new).orElse(ref);
+		}
 	}
 }
