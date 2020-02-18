52599cd6748abb2dbe8e839951b5511e948d681a
yibnl <yassin.kammoun@outlook.com>
2020-02-18 14:25:29 +0100

    (compiler) Extend TypeScopes attribute with type declaration retrieval
    Introducing the capability to retrieve a type declaration from an expression.

--- compiler/src/main/java/se/lth/cs/tycho/attribute/TypeScopes.java
+++ compiler/src/main/java/se/lth/cs/tycho/attribute/TypeScopes.java
@@ -4,13 +4,20 @@ import org.multij.Binding;
 import org.multij.BindingKind;
 import org.multij.Module;
 import org.multij.MultiJ;
+import se.lth.cs.tycho.compiler.SourceUnit;
 import se.lth.cs.tycho.ir.IRNode;
 import se.lth.cs.tycho.ir.NamespaceDecl;
 import se.lth.cs.tycho.ir.decl.Availability;
 import se.lth.cs.tycho.ir.decl.GlobalTypeDecl;
 import se.lth.cs.tycho.ir.decl.TypeDecl;
+import se.lth.cs.tycho.ir.expr.ExprConstruction;
+import se.lth.cs.tycho.ir.expr.ExprVariable;
+import se.lth.cs.tycho.ir.type.NominalTypeExpr;
 import se.lth.cs.tycho.ir.util.ImmutableList;
 import se.lth.cs.tycho.phase.TreeShadow;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public interface TypeScopes {
 
@@ -22,6 +29,8 @@ public interface TypeScopes {
 			.instance();
 
 	ImmutableList<TypeDecl> declarations(IRNode node);
+
+	Optional<TypeDecl> declaration(IRNode node);
 
 	@Module
 	interface Implementation extends TypeScopes {
@@ -58,6 +67,52 @@ public interface TypeScopes {
 							.filter(decl -> ((GlobalTypeDecl) decl).getAvailability().equals(Availability.PUBLIC))
 							.collect(ImmutableList.collector());
 			return ImmutableList.concat(ImmutableList.concat(locals, singles), groups);
+		}
+
+		@Override
+		default Optional<TypeDecl> declaration(IRNode node) {
+			return Optional.empty();
+		}
+
+		default Optional<TypeDecl> declaration(ExprConstruction construction) {
+			return typeDeclarations()
+					.declarations(sourceUnit(construction).getTree())
+					.stream()
+					.map(GlobalTypeDecl.class::cast)
+					.filter(decl -> Objects.equals(decl.getName(), construction.getType()))
+					// name analysis ??? .filter(decl -> decl.getTuples().stream().anyMatch(tuple -> Objects.equals(tuple.getName(), reference.getChild())))
+					.map(TypeDecl.class::cast)
+					.findAny();
+		}
+
+		default Optional<TypeDecl> declaration(ExprVariable var) {
+			return typeDeclarations()
+					.declarations(sourceUnit(var).getTree())
+					.stream()
+					.map(GlobalTypeDecl.class::cast)
+					.filter(decl -> Objects.equals(decl.getName(), var.getVariable().getName()))
+					// name analysis ??? .filter(decl -> decl.getTuples().stream().anyMatch(tuple -> Objects.equals(tuple.getName(), reference.getChild())))
+					.map(TypeDecl.class::cast)
+					.findAny();
+		}
+
+		default Optional<TypeDecl> declaration(NominalTypeExpr type) {
+			return typeDeclarations()
+					.declarations(sourceUnit(type).getTree())
+					.stream()
+					.map(GlobalTypeDecl.class::cast)
+					.filter(decl -> Objects.equals(decl.getName(), type.getName()))
+					// name analysis ??? .filter(decl -> decl.getTuples().stream().anyMatch(tuple -> Objects.equals(tuple.getName(), reference.getChild())))
+					.map(TypeDecl.class::cast)
+					.findAny();
+		}
+
+		default SourceUnit sourceUnit(IRNode node) {
+			return sourceUnit(tree().parent(node));
+		}
+
+		default SourceUnit sourceUnit(SourceUnit unit) {
+			return unit;
 		}
 	}
 }
