abb10ba57e1ce3af5014b9d2cdd2222b531b5039
yibnl <yassin.kammoun@outlook.com>
2020-03-06 16:54:23 +0100

    (platform-generic-c) Add serialization utils for algebraic data types
    Serializing an algebraic data types requires:
    - A 'write' function to write an adt to a char* buffer
    - A 'read' function to read an adt from a char* buffer
    - A 'size' function to compute the serialization size of an adt

--- platform-generic-c/src/main/java/se/lth/cs/tycho/backend/c/AlgebraicTypes.java
+++ platform-generic-c/src/main/java/se/lth/cs/tycho/backend/c/AlgebraicTypes.java
@@ -48,7 +48,7 @@ public interface AlgebraicTypes {
 		emitter().emit("");
 		emitter().emit("%s* read_%s(char *buffer);", code().type(product), product.getName());
 		emitter().emit("");
-		emitter().emit("size_t size_%s(%s);", product.getName(), code().declaration(product, "self"));
+		emitter().emit("size_t size_%s();", product.getName());
 		emitter().emit("");
 	}
 
@@ -90,7 +90,7 @@ public interface AlgebraicTypes {
 		emitter().emit("");
 		emitter().emit("%s* read_%s(char *buffer);", code().type(sum), sum.getName());
 		emitter().emit("");
-		emitter().emit("size_t size_%s(%s);", sum.getName(), code().declaration(sum, "self"));
+		emitter().emit("size_t size_%s();", sum.getName(), code().declaration(sum, "self"));
 		emitter().emit("");
 	}
 
@@ -103,6 +103,9 @@ public interface AlgebraicTypes {
 
 	default void defineType(ProductType product) {
 		defineInit(product);
+		defineWrite(product);
+		defineRead(product);
+		defineSize(product);
 	}
 
 	default void defineInit(ProductType product) {
@@ -117,8 +120,71 @@ public interface AlgebraicTypes {
 		emitter().emit("");
 	}
 
+	default void defineWrite(ProductType product) {
+		String self = "self";
+		String ptr = "ptr";
+		emitter().emit("void write_%s(%s, char *buffer) {", product.getName(), code().declaration(product, self));
+		emitter().increaseIndentation();
+		emitter().emit("char *ptr = buffer;");
+		product.getFields().forEach(field -> {
+			if (field.getType() instanceof AlgebraicType) {
+				emitter().emit("write_%s(%s->%s, %s);", ((AlgebraicType) field.getType()).getName(), self, field.getName(), ptr);
+				emitter().emit("%s += size_%s();", ptr, ((AlgebraicType) field.getType()).getName());
+			} else {
+				emitter().emit("*(%s*) %s = %s->%s;", code().type(field.getType()), ptr, self, field.getName());
+				emitter().emit("%s = (char*)((%s*) %s + 1);", ptr, code().type(field.getType()), ptr);
+			}
+		});
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
+	}
+
+	default void defineRead(ProductType product) {
+		String self = "self";
+		String ptr = "ptr";
+		emitter().emit("%s* read_%s(char *buffer) {", code().type(product), product.getName());
+		emitter().increaseIndentation();
+		emitter().emit("char *%s = buffer;", ptr);
+		emitter().emit("%s = calloc(1, sizeof(%s_t));", code().declaration(product, self), product.getName());
+		product.getFields().forEach(field -> {
+			if (field.getType() instanceof AlgebraicType) {
+				emitter().emit("%s->%s = read_%s(%s);", self, field.getName(), ((AlgebraicType) field.getType()).getName(), ptr);
+				emitter().emit("%s += size_%s();", ptr, ((AlgebraicType) field.getType()).getName());
+			} else {
+				emitter().emit("%s->%s = *(%s*) %s;", self, field.getName(), code().type(field.getType()), ptr);
+				emitter().emit("%s = (char*)((%s*) %s + 1);", ptr, code().type(field.getType()), ptr);
+			}
+		});
+		emitter().emit("return %s;", self);
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
+	}
+
+	default void defineSize(ProductType product) {
+		String size = "size";
+		emitter().emit("size_t size_%s() {", product.getName());
+		emitter().increaseIndentation();
+		emitter().emit("size_t %s = 0;", size);
+		product.getFields().forEach(field -> {
+			if (field.getType() instanceof AlgebraicType) {
+				emitter().emit("%s += size_%s();", size, ((AlgebraicType) field.getType()).getName());
+			} else {
+				emitter().emit("%s += sizeof(%s);", size, code().type(field.getType()));
+			}
+		});
+		emitter().emit("return %s;", size);
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
+	}
+
 	default void defineType(SumType sum) {
 		defineInit(sum);
+		defineWrite(sum);
+		defineRead(sum);
+		defineSize(sum);
 	}
 
 	default void defineInit(SumType sum) {
@@ -136,6 +202,109 @@ public interface AlgebraicTypes {
 			emitter().emit("}");
 			emitter().emit("");
 		});
+	}
+
+	default void defineWrite(SumType sum) {
+		String self = "self";
+		String ptr = "ptr";
+		emitter().emit("void write_%s(%s, char *buffer) {", sum.getName(), code().declaration(sum, self));
+		emitter().increaseIndentation();
+		emitter().emit("char *%s = buffer;", ptr);
+		emitter().emit("*(enum %s_tag_t*) %s = %s->tag;", sum.getName(), ptr, self);
+		emitter().emit("%s = (char*)((enum %s_tag_t*) ptr + 1);", ptr, sum.getName(), ptr);
+		emitter().emit("switch (%s->tag) {", self);
+		emitter().increaseIndentation();
+		sum.getVariants().forEach(variant -> {
+			emitter().emit("case tag_%s_%s:", sum.getName(), variant.getName());
+			emitter().increaseIndentation();
+			variant.getFields().forEach(field -> {
+				if (field.getType() instanceof AlgebraicType) {
+					emitter().emit("write_%s(%s->data.%s.%s, %s);", ((AlgebraicType) field.getType()).getName(), self, variant.getName(), field.getName(), ptr);
+					emitter().emit("%s += size_%s();", ptr, ((AlgebraicType) field.getType()).getName());
+				} else {
+					emitter().emit("*(%s*) %s = %s->data.%s.%s;", code().type(field.getType()), ptr, self, variant.getName(), field.getName());
+					emitter().emit("%s = (char*)((%s*) %s + 1);", ptr, code().type(field.getType()), ptr);
+				}
+			});
+			emitter().emit("break;");
+			emitter().decreaseIndentation();
+		});
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
+	}
+
+	default void defineRead(SumType sum) {
+		String self = "self";
+		String ptr = "ptr";
+		emitter().emit("%s* read_%s(char *buffer) {", code().type(sum), sum.getName());
+		emitter().increaseIndentation();
+		emitter().emit("char *%s = buffer;", ptr);
+		emitter().emit("%s = calloc(1, sizeof(%s_t));", code().declaration(sum, self), sum.getName());
+		emitter().emit("%s->tag = *(enum %s_tag_t*) %s;", self,  sum.getName(), ptr);
+		emitter().emit("%s = (char*)((enum %s_tag_t*) ptr + 1);", ptr, sum.getName(), ptr);
+		emitter().emit("switch (%s->tag) {", self);
+		emitter().increaseIndentation();
+		sum.getVariants().forEach(variant -> {
+			emitter().emit("case tag_%s_%s:", sum.getName(), variant.getName());
+			emitter().increaseIndentation();
+			variant.getFields().forEach(field -> {
+				if (field.getType() instanceof AlgebraicType) {
+					emitter().emit("%s->data.%s.%s = read_%s(%s);", self, variant.getName(), field.getName(), ((AlgebraicType) field.getType()).getName(), ptr);
+					emitter().emit("%s += size_%s();", ptr, ((AlgebraicType) field.getType()).getName());
+				} else {
+					emitter().emit("%s->data.%s.%s = *(%s*) %s;", self, variant.getName(), field.getName(), code().type(field.getType()), ptr);
+					emitter().emit("%s = (char*)((%s*) %s + 1);", ptr, code().type(field.getType()), ptr);
+				}
+			});
+			emitter().emit("break;");
+			emitter().decreaseIndentation();
+		});
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("return self;");
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
+	}
+
+	default void defineSize(SumType sum) {
+		String size = "size";
+		String max = "max";
+		String tag = "tag";
+		emitter().emit("size_t size_%s() {", sum.getName());
+		emitter().increaseIndentation();
+		emitter().emit("size_t %s = 0;", max);
+		emitter().emit("for (int %s = 0; %s < %s; ++%s) {", tag, tag, sum.getVariants().size(), tag);
+		emitter().increaseIndentation();
+		emitter().emit("size_t %s = 0;", size);
+		emitter().emit("switch (%s) {", tag);
+		emitter().increaseIndentation();
+		sum.getVariants().forEach(variant -> {
+			emitter().emit("case tag_%s_%s:", sum.getName(), variant.getName());
+			emitter().increaseIndentation();
+			variant.getFields().forEach(field -> {
+				if (field.getType() instanceof AlgebraicType) {
+					emitter().emit("%s += size_%s();", size, ((AlgebraicType) field.getType()).getName());
+				} else {
+					emitter().emit("%s += sizeof(%s);", size, code().type(field.getType()));
+				}
+			});
+			emitter().emit("break;");
+			emitter().decreaseIndentation();
+		});
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("%s = %s < %s ? %s : %s;", max, max, size, size, max);
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("%s += sizeof(enum %s_tag_t);", max, sum.getName());
+		emitter().emit("return %s;", max);
+		emitter().decreaseIndentation();
+		emitter().emit("}");
+		emitter().emit("");
 	}
 
 	default String type(AlgebraicType type) {
