df06f391ed0a3d1a2eb087ff9c5dbb5e1dfe8c4b
yibnl <yassin.kammoun@outlook.com>
2020-04-16 10:26:25 +0200

    (core, compiler, app-test) Change syntax of pattern expression
    Pattern expression requires now parentheses surrounding the expression. A consequence of this change is the parsing of pattern wildcard, pattern binding and pattern deconstruction from the grammar itself rather than relying on a subsequent compilation phase. Since the gramamr now has production rules for all supported patterns, the pattern resolving phase only needs to resolve pattern bindings to pattern deconstructions if the underlying name refers to a type or a variant without fields.

--- compiler/src/main/java/se/lth/cs/tycho/phase/ResolvePatternsPhase.java
+++ compiler/src/main/java/se/lth/cs/tycho/phase/ResolvePatternsPhase.java
@@ -8,16 +8,8 @@ import se.lth.cs.tycho.attribute.TypeScopes;
 import se.lth.cs.tycho.compiler.CompilationTask;
 import se.lth.cs.tycho.compiler.Context;
 import se.lth.cs.tycho.ir.IRNode;
-import se.lth.cs.tycho.ir.decl.PatternVarDecl;
-import se.lth.cs.tycho.ir.expr.ExprApplication;
-import se.lth.cs.tycho.ir.expr.ExprVariable;
-import se.lth.cs.tycho.ir.expr.Expression;
-import se.lth.cs.tycho.ir.expr.pattern.Pattern;
 import se.lth.cs.tycho.ir.expr.pattern.PatternDeconstruction;
-import se.lth.cs.tycho.ir.expr.pattern.PatternExpression;
 import se.lth.cs.tycho.ir.expr.pattern.PatternBinding;
-import se.lth.cs.tycho.ir.expr.pattern.PatternWildcard;
-import se.lth.cs.tycho.ir.util.ImmutableList;
 import se.lth.cs.tycho.reporting.CompilationException;
 
 import java.util.Collections;
@@ -48,26 +40,9 @@ public class ResolvePatternsPhase implements Phase {
 			return node.transformChildren(this);
 		}
 
-		default IRNode apply(PatternExpression pattern) {
-			Expression expr = pattern.getExpression();
-			if (expr instanceof ExprVariable) {
-				ExprVariable exprVariable = (ExprVariable) expr;
-				if (exprVariable.getVariable().getName().equals("_")) {
-					return new PatternWildcard(exprVariable);
-				} else if (typeScopes().construction(exprVariable).isPresent()) {
-					return new PatternDeconstruction(exprVariable, exprVariable.getVariable().getName(), Collections.emptyList());
-				} else {
-					PatternVarDecl decl = new PatternVarDecl(exprVariable.getVariable().getName());
-					decl.setPosition(exprVariable.getFromLineNumber(), exprVariable.getFromColumnNumber(), exprVariable.getToLineNumber(), exprVariable.getToColumnNumber());
-					return new PatternBinding(exprVariable, decl);
-				}
-			}
-			if (expr instanceof ExprApplication && ((ExprApplication) expr).getFunction() instanceof ExprVariable) {
-				ExprApplication application = (ExprApplication) expr;
-				ExprVariable exprVariable = (ExprVariable) application.getFunction();
-				if (typeScopes().construction(exprVariable).isPresent()) {
-					return new PatternDeconstruction(application, exprVariable.getVariable().getName(), application.getArgs().stream().map(arg -> apply(new PatternExpression(arg, arg))).map(Pattern.class::cast).collect(ImmutableList.collector()));
-				}
+		default IRNode apply(PatternBinding pattern) {
+			if (typeScopes().construction(pattern.getDeclaration()).isPresent()) {
+				return new PatternDeconstruction(pattern, pattern.getDeclaration().getName(), Collections.emptyList());
 			}
 			return pattern;
 		}
