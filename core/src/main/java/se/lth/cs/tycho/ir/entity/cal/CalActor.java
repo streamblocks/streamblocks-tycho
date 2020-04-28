bb14910c8380733697e41adb474d783a995a5170
yibnl <yassin.kammoun@outlook.com>
2020-04-28 17:44:35 +0200

    (core, compiler) Add a phase to assign priorities to actions
    Priorities of actions are based on the pattern type of the inputs patterns. The more precise a pattern is, the higher the priority is.

--- core/src/main/java/se/lth/cs/tycho/ir/entity/cal/CalActor.java
+++ core/src/main/java/se/lth/cs/tycho/ir/entity/cal/CalActor.java
@@ -226,4 +226,12 @@ public class CalActor extends Entity {
  			return new CalActor(this, typeParameters, valueParameters, typeDecls, varDecls, inputPorts, outputPorts, ImmutableList.from(initializers), actions, scheduleFSM, process, priorities, invariants);
 		}
 	}
+
+	public CalActor withPriorities(List<ImmutableList<QID>> priorities) {
+		if (Lists.sameElements(this.priorities, priorities)) {
+			return this;
+		} else {
+			return new CalActor(this, typeParameters, valueParameters, typeDecls, varDecls, inputPorts, outputPorts, initializers, actions, scheduleFSM, process, ImmutableList.from(priorities), invariants);
+		}
+	}
 }
