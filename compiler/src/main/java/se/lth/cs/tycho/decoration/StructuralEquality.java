a63fb279a129db5e7c3b5247a14df80fcea5cc5d
yibnl <yassin.kammoun@outlook.com>
2020-05-08 11:55:31 +0200

    (compiler) Fix pattern matching analysis on expressions
    Considers structural equality of singletons on unreachability analysis.

--- compiler/src/main/java/se/lth/cs/tycho/decoration/StructuralEquality.java
+++ compiler/src/main/java/se/lth/cs/tycho/decoration/StructuralEquality.java
@@ -2,15 +2,24 @@ package se.lth.cs.tycho.decoration;
 
 import org.multij.Module;
 import org.multij.MultiJ;
+import se.lth.cs.tycho.ir.TypeParameter;
+import se.lth.cs.tycho.ir.ValueParameter;
 import se.lth.cs.tycho.ir.Variable;
 import se.lth.cs.tycho.ir.expr.ExprApplication;
 import se.lth.cs.tycho.ir.expr.ExprBinaryOp;
+import se.lth.cs.tycho.ir.expr.ExprField;
 import se.lth.cs.tycho.ir.expr.ExprGlobalVariable;
+import se.lth.cs.tycho.ir.expr.ExprIf;
 import se.lth.cs.tycho.ir.expr.ExprIndexer;
+import se.lth.cs.tycho.ir.expr.ExprList;
 import se.lth.cs.tycho.ir.expr.ExprLiteral;
+import se.lth.cs.tycho.ir.expr.ExprTypeAssertion;
+import se.lth.cs.tycho.ir.expr.ExprTypeConstruction;
 import se.lth.cs.tycho.ir.expr.ExprUnaryOp;
 import se.lth.cs.tycho.ir.expr.ExprVariable;
 import se.lth.cs.tycho.ir.expr.Expression;
+import se.lth.cs.tycho.ir.type.NominalTypeExpr;
+import se.lth.cs.tycho.ir.type.TypeExpr;
 
 import java.util.Iterator;
 import java.util.List;
@@ -61,6 +70,48 @@ public final class StructuralEquality {
 
 		default boolean eq(ExprIndexer a, ExprIndexer b) {
 			return eq(a.getStructure(), b.getStructure()) && eq(a.getIndex(), b.getIndex());
+		}
+
+		default boolean eq(ExprField a, ExprField b) {
+			return eq(a.getStructure(), b.getStructure())
+					&& Objects.equals(a.getField().getName(), b.getField().getName());
+		}
+
+		default boolean eq(ExprIf a, ExprIf b) {
+			return eq(a.getCondition(), b.getCondition())
+					&& eq(a.getThenExpr(), b.getThenExpr())
+					&& eq(a.getElseExpr(), b.getElseExpr());
+		}
+
+		default boolean eq(ExprTypeConstruction a, ExprTypeConstruction b) {
+			return Objects.equals(a.getConstructor(), b.getConstructor())
+					&& listEquals(a.getArgs(), b.getArgs(), this::eq);
+		}
+
+		default boolean eq(ExprTypeAssertion a, ExprTypeAssertion b) {
+			return eq(a.getType(), b.getType()) && eq(a.getExpression(), b.getExpression());
+		}
+
+		default boolean eq(ExprList a, ExprList b) {
+			return listEquals(a.getElements(), b.getElements(), this::eq);
+		}
+
+		default boolean eq(TypeExpr a, TypeExpr b) {
+			return false;
+		}
+
+		default boolean eq(NominalTypeExpr a, NominalTypeExpr b) {
+			return Objects.equals(a.getName(), b.getName())
+					&& listEquals(a.getTypeParameters(), b.getTypeParameters(), this::eq)
+					&& listEquals(a.getValueParameters(), b.getValueParameters(), this::eq);
+		}
+
+		default boolean eq(TypeParameter a, TypeParameter b) {
+			return Objects.equals(a.getName(), b.getName()) && eq(a.getValue(), b.getValue());
+		}
+
+		default boolean eq(ValueParameter a, ValueParameter b) {
+			return Objects.equals(a.getName(), b.getName()) && eq(a.getValue(), b.getValue());
 		}
 
 		default <T> boolean listEquals(List<T> a, List<T> b, BiPredicate<T, T> equals) {
