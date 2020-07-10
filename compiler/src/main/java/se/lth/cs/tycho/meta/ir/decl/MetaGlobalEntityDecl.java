22a540e09619c47ce3730f471f3103288e843e4f
yibnl <yassin.kammoun@outlook.com>
2020-07-10 11:44:10 +0200

    (compiler) Add missing node utils to MetaGlobalEntityDecl

--- compiler/src/main/java/se/lth/cs/tycho/meta/ir/decl/MetaGlobalEntityDecl.java
+++ compiler/src/main/java/se/lth/cs/tycho/meta/ir/decl/MetaGlobalEntityDecl.java
@@ -1,9 +1,12 @@
 package se.lth.cs.tycho.meta.ir.decl;
 
+import se.lth.cs.tycho.ir.IRNode;
 import se.lth.cs.tycho.ir.decl.GlobalEntityDecl;
+import se.lth.cs.tycho.ir.util.Lists;
 import se.lth.cs.tycho.meta.core.MetaParameter;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MetaGlobalEntityDecl extends GlobalEntityDecl {
 
@@ -14,7 +17,26 @@ public class MetaGlobalEntityDecl extends GlobalEntityDecl {
 		this.parameters = parameters;
 	}
 
+	public MetaGlobalEntityDecl copy(List<MetaParameter> parameters) {
+		if (Lists.sameElements(getParameters(), parameters)) {
+			return this;
+		} else {
+			return new MetaGlobalEntityDecl(parameters, this);
+		}
+	}
+
 	public List<MetaParameter> getParameters() {
 		return parameters;
+	}
+
+	@Override
+	public void forEachChild(Consumer<? super IRNode> action) {
+		super.forEachChild(action);
+		getParameters().forEach(action);
+	}
+
+	@Override
+	public GlobalEntityDecl transformChildren(Transformation transformation) {
+		return copy(transformation.mapChecked(MetaParameter.class, getParameters()));
 	}
 }
