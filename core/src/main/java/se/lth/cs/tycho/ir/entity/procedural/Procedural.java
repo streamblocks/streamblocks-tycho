4f7865d7b4a6d0838ed446393b8bd66fcf66a4ba
Endri Bezati <endri.bezati@gmail.com>
2020-10-15 18:30:56 +0200

    (core) Starting Precedural entity. The idea is to transform the Actor Machine to Procedural one. A Procedural contains the scopes and everything on the AM is transformed to Function.

---
+++ core/src/main/java/se/lth/cs/tycho/ir/entity/procedural/Procedural.java
@@ -0,0 +1,82 @@
+package se.lth.cs.tycho.ir.entity.procedural;
+
+import se.lth.cs.tycho.ir.Annotation;
+import se.lth.cs.tycho.ir.IRNode;
+import se.lth.cs.tycho.ir.decl.ParameterTypeDecl;
+import se.lth.cs.tycho.ir.decl.ParameterVarDecl;
+import se.lth.cs.tycho.ir.entity.Entity;
+import se.lth.cs.tycho.ir.entity.PortDecl;
+import se.lth.cs.tycho.ir.entity.am.Scope;
+import se.lth.cs.tycho.ir.util.ImmutableList;
+import se.lth.cs.tycho.ir.util.Lists;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Procedural extends Entity {
+    public Procedural(IRNode original, List<Annotation> annotations, List<PortDecl> inputPorts, List<PortDecl> outputPorts,
+                      List<ParameterTypeDecl> typeParameters, List<ParameterVarDecl> valueParameters, List<Scope> scopes, List<Function> functions) {
+        super(original, annotations, inputPorts, outputPorts, typeParameters, valueParameters);
+        this.scopes = ImmutableList.from(scopes);
+        this.functions = ImmutableList.from(functions);
+    }
+
+
+    public Procedural copy(List<Annotation> annotations, List<PortDecl> inputPorts, List<PortDecl> outputPorts, List<ParameterTypeDecl> typeParameters,
+                           List<ParameterVarDecl> valueParameters, List<Scope> scopes, List<Function> functions) {
+        if (Lists.sameElements(this.annotations, annotations)
+                && Lists.sameElements(this.inputPorts, inputPorts)
+                && Lists.sameElements(this.outputPorts, outputPorts)
+                && Lists.sameElements(this.typeParameters, typeParameters)
+                && Lists.sameElements(this.valueParameters, valueParameters)
+                && Lists.sameElements(this.scopes, scopes)
+                && Lists.sameElements(this.functions, functions)) {
+            return this;
+        }
+        return new Procedural(this, annotations, inputPorts, outputPorts, typeParameters, valueParameters, scopes, functions);
+    }
+
+
+    @Override
+    public Entity withTypeParameters(List<ParameterTypeDecl> typeParameters) {
+        return null;
+    }
+
+    @Override
+    public Entity withValueParameters(List<ParameterVarDecl> valueParameters) {
+        return null;
+    }
+
+    private final ImmutableList<Scope> scopes;
+    private final ImmutableList<Function> functions;
+
+    @Override
+    public void forEachChild(Consumer<? super IRNode> action) {
+        super.forEachChild(action);
+        functions.forEach(action);
+    }
+
+    @Override
+    public Procedural transformChildren(Transformation transformation) {
+        return copy(
+                (ImmutableList) annotations.map(transformation),
+                (ImmutableList) inputPorts.map(transformation),
+                (ImmutableList) outputPorts.map(transformation),
+                (ImmutableList) typeParameters.map(transformation),
+                (ImmutableList) valueParameters.map(transformation),
+                (ImmutableList) scopes.map(transformation),
+                (ImmutableList) functions.map(transformation)
+        );
+    }
+
+    @Override
+    public Procedural clone() {
+        return (Procedural) super.clone();
+    }
+
+    @Override
+    public Procedural deepClone() {
+        return (Procedural) super.deepClone();
+    }
+
+}
