2c2526a2df06a04155e1e1fe70a1a618d5ba4156
Vincent <vincentpellenc@gmail.com>
2021-04-05 11:34:09 +0200

    create intermediate SSABlock class

--- compiler/src/main/java/se/lth/cs/tycho/phase/SsaPhase.java
+++ compiler/src/main/java/se/lth/cs/tycho/phase/SsaPhase.java
@@ -1,5 +1,6 @@
 package se.lth.cs.tycho.phase;
 
+import jdk.vm.ci.meta.Local;
 import org.multij.Module;
 import org.multij.MultiJ;
 import se.lth.cs.tycho.attribute.VariableDeclarations;
@@ -9,6 +10,7 @@ import se.lth.cs.tycho.ir.IRNode;
 import se.lth.cs.tycho.ir.Variable;
 import se.lth.cs.tycho.ir.decl.LocalVarDecl;
 import se.lth.cs.tycho.ir.decl.ParameterVarDecl;
+import se.lth.cs.tycho.ir.decl.TypeDecl;
 import se.lth.cs.tycho.ir.decl.VarDecl;
 import se.lth.cs.tycho.ir.entity.am.Transition;
 import se.lth.cs.tycho.ir.expr.*;
@@ -23,6 +25,7 @@ import se.lth.cs.tycho.util.Pair;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -97,10 +100,42 @@ public class SsaPhase implements Phase {
 
     }
 
+    /*public StmtBlock fillBlock(StmtBlock block) {
+
+        return null;
+    }*/
+
+    public List<Statement> fillBlock(List<Statement> statements, SSABlock block) {
+
+        int splitIndex = 0;
+        for (Statement statement: statements) {
+            if (statement instanceof StmtAssignment) {
+                StmtAssignment assignment = (StmtAssignment) statement;
+
+                // To be fixed
+                Variable lValue = ((LValueVariable) assignment.getLValue()).getVariable();
+                LocalVarDecl originalDecl = block.getOriginalDefinition(lValue);
+                //String newNumberedVar = new Variable(originalDecl.getName() + "_" + block.getVariableNumber(lValue));
+
+                if (assignment.getExpression() instanceof ExprVariable) {
+                    Variable rValue = ((ExprVariable) assignment.getExpression()).getVariable();
+                    Expression expr = readVariable(rValue, block);
+                    writeVariable(lValue, block, expr);
+                } else {
+                    writeVariable(lValue, block, assignment.getExpression());
+                }
+            }
+
+            splitIndex++;
+        }
+
+        return null;
+    }
+
 
 // ------------------------------------------------------ SSA algorithm core -------------------------------------------------------------
 
-    public void writeVariable(Variable variable, StmtBlock block, Expression value) {
+    /*public void writeVariable(Variable variable, StmtBlock block, Expression value) {
         currentDef.putIfAbsent(variable, new HashMap<>());
         Map<StmtBlock, Expression> currentDefVar = currentDef.get(variable);
         currentDefVar.put(block, value);
@@ -115,6 +150,84 @@ public class SsaPhase implements Phase {
 
     public Expression readVariableRecursive(Variable variable, StmtBlock block) {
         throw new UnsupportedOperationException();
+    }*/
+
+    public void writeVariable(Variable variable, SSABlock block, Expression value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Expression readVariable(Variable variable, SSABlock block) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Expression readVariableRecursive(Variable variable, SSABlock block) {
+        throw new UnsupportedOperationException();
+    }
+
+// ------------------------------------------------------ SSA data structure -------------------------------------------------------------
+
+    private class SSABlock extends Statement {
+
+        private List<TypeDecl> typeDecls;
+        private LinkedList<LocalVarDecl> varDecls;
+        private final List<SSABlock> predecessors = new LinkedList<>();
+        private final Map<Variable, Expression> currentDef = new HashMap<>();
+        private final Map<Variable, Integer> currentNumber = new HashMap<>();
+        private List<Statement> statements;
+        private List<StmtPhi> phis;
+        private boolean sealed = false;
+
+        private SSABlock(SSABlock original, List<TypeDecl> typeDecls, LinkedList<LocalVarDecl> varDecls) {
+            super(original);
+            this.typeDecls = typeDecls;
+            this.varDecls = varDecls;
+        }
+
+        public SSABlock(List<TypeDecl> typeDecls, List<LocalVarDecl> varDecls) {
+            this(null, typeDecls, new LinkedList<>(varDecls));
+        }
+
+        public SSABlock() {
+            this(new LinkedList<>(), new LinkedList<>());
+        }
+
+        public void seal() {
+            sealed = true;
+        }
+
+        public LocalVarDecl getOriginalDefinition(Variable variable) {
+            //varDecls.stream().fin
+            return null;
+        }
+
+        public int getVariableNumber(Variable variable) {
+            if (currentNumber.containsKey(variable)) {
+                return currentNumber.get(variable);
+            }
+            return predecessors.stream().map(blk -> blk.getVariableNumber(variable)).max(Integer::compare).get();
+        }
+
+        public void addDecl(LocalVarDecl decl) {
+            varDecls.add(decl);
+        }
+
+        public void setStatements(List<Statement> statements) {
+            this.statements = statements;
+        }
+
+        public StmtBlock getStmtBlock() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void forEachChild(Consumer<? super IRNode> action) {
+
+        }
+
+        @Override
+        public Statement transformChildren(Transformation transformation) {
+            return null;
+        }
     }
 
 }
