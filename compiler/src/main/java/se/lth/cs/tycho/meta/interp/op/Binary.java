a4d49c4c7fffe6eb2c787a65bc0abf89e0f4ec0e
Endri Bzati <endri.bezati@huawei.com>
2022-09-26 15:42:59 +0200

    (Meta) interpreter, remove double quotes on string concatenation.

--- compiler/src/main/java/se/lth/cs/tycho/meta/interp/op/Binary.java
+++ compiler/src/main/java/se/lth/cs/tycho/meta/interp/op/Binary.java
@@ -184,7 +184,7 @@ public interface Binary {
 	}
 
 	default Value apply(OperatorPlus op, ValueString lhs, ValueString rhs) {
-		return new ValueString(lhs.string() + rhs.string());
+		return new ValueString(lhs.string().replaceAll("\"", "") + rhs.string().replaceAll("\"", ""));
 	}
 
 	default Value apply(OperatorPlus op, ValueString lhs, ValueLong rhs) {
