6d0942b9027278b5c704b3cadaf9ec3f9ce0be69
Gareth Callanan <gcallanan@gmail.com>
2024-07-15 11:12:25 +0200

    Added a bypass for the actor machine steps.

--- compiler/src/main/java/se/lth/cs/tycho/phase/CalToAmPhase.java
+++ compiler/src/main/java/se/lth/cs/tycho/phase/CalToAmPhase.java
@@ -7,13 +7,34 @@ import se.lth.cs.tycho.compiler.Transformations;
 import se.lth.cs.tycho.ir.Annotation;
 import se.lth.cs.tycho.ir.entity.cal.CalActor;
 import se.lth.cs.tycho.ir.util.ImmutableList;
+import se.lth.cs.tycho.settings.Configuration;
+import se.lth.cs.tycho.settings.OnOffSetting;
+import se.lth.cs.tycho.settings.Setting;
 import se.lth.cs.tycho.transformation.cal2am.CalToAm;
 import se.lth.cs.tycho.transformation.cal2am.KnowledgeRemoval;
-import se.lth.cs.tycho.settings.Setting;
 
 import java.util.List;
 
 public class CalToAmPhase implements Phase {
+    public static Setting<Boolean> bypassAmGeneration = new OnOffSetting() {
+        @Override
+        public String getKey() {
+            return "bypass-AM-generation";
+        }
+
+        @Override
+        public String getDescription() {
+            return "Bypass the conversion of a CAL actors to Actor machines. This requires that a backend is being " +
+                    "used that is able to generate code from normal CAL actors, not AMs. If a backend is used that " +
+                    "does not support this, errors will be thrown or else strange code will be generated.";
+        }
+
+        @Override
+        public Boolean defaultValue(Configuration configuration) {
+            return false;
+        }
+    };
+
     @Override
     public String getDescription() {
         return "Translates all Cal actors to actor machines";
@@ -22,7 +43,13 @@ public class CalToAmPhase implements Phase {
     @Override
     public CompilationTask execute(CompilationTask task, Context context) {
         return Transformations.transformEntityDecls(task, decl -> {
-            boolean hasNoActorMachine = Annotation.hasAnnotationWithName("no_actor_machine", decl.getEntity().getAnnotations());
+            // Note by Gareth Callanan on 2024/07/15: It would be nice if each actor could be annotated to tell
+            // the compiler not to generate an actor machine, but I have not had time to implement it. So currently
+            // either all actors have actor machines or none. As of this date, only the vitis backend supports
+            // non-AM actors, so this can cause problem when paritioning the network between hardware and software.
+            // boolean hasNoActorMachine = Annotation.hasAnnotationWithName("no_actor_machine",
+            //        decl.getEntity().getAnnotations());
+            boolean hasNoActorMachine = context.getConfiguration().get(bypassAmGeneration);
             if (decl.getEntity() instanceof CalActor && !hasNoActorMachine) {
                 CalToAm translator = new CalToAm((CalActor) decl.getEntity(), context.getConfiguration(),
                         task.getModule(ConstantEvaluator.key),
@@ -43,7 +70,8 @@ public class CalToAmPhase implements Phase {
     public List<Setting<?>> getPhaseSettings() {
         return ImmutableList.of(
                 KnowledgeRemoval.forgetOnExec,
-                KnowledgeRemoval.forgetOnWait
+                KnowledgeRemoval.forgetOnWait,
+                bypassAmGeneration
         );
     }
 }
